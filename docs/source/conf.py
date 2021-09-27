1cb3a5cf4579af1f8f64e8af8ef7c6ab4f05c48b
Sebastian Schönnenbeck <sebastian.schoennenbeck@comma-soft.com>
2021-09-27 16:41:23 +0200

    Bump version: 0.2.1 → 0.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Sebastian Schönnenbeck"
 author = "Sebastian Schönnenbeck"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.2.2"
 
 
 # -- General configuration ---------------------------------------------------
